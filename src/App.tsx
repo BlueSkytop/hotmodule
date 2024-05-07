f160969966f3c069230f166aace8e586cd70f797
BlueSkytop <olgakutnja@gmail.com>
2024-05-07 23:01:16 +0300

    props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 import './App.css';
+import Character from "./components/character/Character";
 
 function App() {
   return (
    <>
-   </>
+       <Character name={'Bart Simpson'}
+                  image={'https://seeklogo.com/images/B/bart-simpson-logo-A3BCF65934-seeklogo.com.png'}
+       />
+       <Character name={'Homer Simpson'}
+                  image={'https://static.miraheze.org/loathsomecharacterswiki/thumb/3/32/Mmm_pork_rinds.png/300px-Mmm_pork_rinds.png'}
+       />
+       </>
   );
 }
 
