675295cc6cf14e2d2a705c696f500f4f90ceae56
BlueSkytop <olgakutnja@gmail.com>
2024-05-08 15:31:18 +0300

    simpsons

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,17 @@ function App() {
        <Character name={'Homer Simpson'}
                   image={'https://static.miraheze.org/loathsomecharacterswiki/thumb/3/32/Mmm_pork_rinds.png/300px-Mmm_pork_rinds.png'}
        />
+       <Character
+           name={'Marge Simpson'}
+           image={'https://icons.veryicon.com/png/Movie%20%26%20TV/Simpsons%204/Marge%20Simpson.png'}>
+               Marjorie Jacqueline "Marge" Simpson(Born March 19) is the homemaker and ' +
+               'matriarch of the Simpson family. She is also one of the five main characters in The Simpsons TV series. ' +
+               'She and her husband Homer have three children: Bart, Lisa, and Maggie. Marge is the moralistic force in ' +
+               'her family and often provides a raspy and grounding voice in the midst of her family\'s antics by trying ' +
+               'to maintain order in the Simpson household. Aside from her duties at home, Marge has flirted briefly with' +
+               'a number of careers ranging from a police officer to an anti-violence activist.
+
+       </Character>
        </>
   );
 }
--- src/components/character/Character.tsx
+++ src/components/character/Character.tsx
@@ -1,17 +1,21 @@
-import React, {FC} from 'react';
+import React, {FC, ReactNode} from 'react';
 
 
 interface IProps{
     name:string;
     image:string
 }
-const Character:FC<IProps> = ({name, image}) => {
+
+type PropsWithChildren<T> = T &{children?: React.ReactNode};
+
+const Character:FC<PropsWithChildren<IProps>> = ({name, image, children}) => {
 
     return (
             <div id={'block'}>
                 <div id={'info'}>
                 <h2>{name}</h2>
                 <img src={image} alt={name}/>
+                    <h5>{children}</h5>
                 </div>
             </div>
     );
