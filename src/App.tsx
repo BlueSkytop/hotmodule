42b5b19a5b8f0f6a66ff00452433a92bf75086b8
BlueSkytop <olgakutnja@gmail.com>
2024-05-10 02:22:55 +0300

    homework1 Products

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,13 @@
-import React, {FC, useEffect, useState} from 'react';
-
-import './App.css';
+import React from 'react';
+import './App.css'
 import Products from "./components/product/Products";
-import {IProduct} from "./components/product/IProduct";
 
-
-const App:FC=() => {
-    const [products, setProducts] = useState<IProduct[]>([]);
-    useEffect(() =>{
-        fetch('https://dummyjson.com/docs/products/')
-            .then(value => value.json())
-            .then(value => {setProducts(value);
-            });
-
-        return() => {
-            console.log('start products');
-        }
-    }, []);
-
-    return(
-        <>
-            {
-                products.map(({id, title, description, price, discountPercentage,
-                                  rating, stock, brand, category, thumbnail, images},
-                              index) =>
-                <Products
-                   key={index}
-                   id={id}
-                   title={title}
-                   description={description}
-                   price={price}
-                   discountPercentage={discountPercentage}
-                   rating={rating}
-                   stock={stock}
-                   brand={brand}
-                   category={category}
-                   thumbnail={thumbnail}
-                   images={images}
-                ></Products>
-                )
-            }
-        </>
+const App = () => {
+    return (
+        <div>
+           <Products/>
+        </div>
     );
 };
 
-export {App}
 export default App;
